69d317123fb7ac050c1b0de5a3ff84bed37d4d9b
FridahWatetu <fridahwatetumuthoni@gmail.com>
2023-01-17 20:06:43 +0300

    1:00:00

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -2,21 +2,25 @@ import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postSlice';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons'
+
 
 const PostList = () => {
     const posts = useSelector(selectAllPosts)
-    console.log(posts)
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
   return (
       <div>
           <h2>Posts</h2>
           {
-              posts.map((post) => {
+              orderedPosts.map((post) => {
                   return (
                       <article key={post.id}>
                           <h3>{post.title}</h3>
                           <PostAuthor userId={post.user} />
                            <TimeAgo timestamp={post.date}/> 
                           <p>{ post.content.substring(0,100)}...</p>
+                          <ReactionButtons post={post}/>
                       </article>
                   )
               })
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -2,8 +2,22 @@ import { createSlice, nanoid } from '@reduxjs/toolkit'
 import {sub} from 'date-fns'
 
 const initialState = [
-    { id: '1',title: 'Learning Redux Toolkit', content: 'Ive heard good things' , date:sub(new Date(),{minutes:10}).toISOString()},
-    {id:'2',title:'Learning Slices.....',content:'The more i say slice, the more i want pizza',date:sub(new Date(),{minutes:5}).toISOString()},
+    { id: '1',title: 'Learning Redux Toolkit', content: 'Ive heard good things' , date:sub(new Date(),{minutes:10}).toISOString(),reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+},
+    {id:'2',title:'Learning Slices.....',content:'The more i say slice, the more i want pizza',date:sub(new Date(),{minutes:5}).toISOString(),reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+},
 ]
 
 const postsSlice= createSlice({
@@ -22,14 +36,28 @@ const postsSlice= createSlice({
                         title: post.title,
                         content: post.content,
                         date:new Date().toISOString(),
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+        }
                     }
                 }
+            }
+        },
+        reactionAdded:(state,action)=>{
+            const{postId,reaction}=action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if(existingPost){
+                existingPost.reactions[reaction]++
             }
         }
     }
 })
 
 export const selectAllPosts = (state) => state.posts
-export const {postAdded}=postsSlice.actions
+export const {postAdded,reactionAdded}=postsSlice.actions
 
 export default postsSlice.reducer
